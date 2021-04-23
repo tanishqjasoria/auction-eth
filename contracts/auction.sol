82f9dcfb9ccf62752488ccce40139a947ba5904e
Tanishq Jasoria <jasoriatanishq@gmail.com>
2021-04-23 13:17:46 +0530

    create a auction for a car

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -43,3 +43,47 @@ contract Auction {
     event CanceledEvent(String message, uint256 time);
 
 }
+
+
+contract CarAuction is Auction {
+    constructor(uint _bidding_time, address _owner, string _brand, string _RNumber ) {
+        auction_owner = _owner;
+        auction_start = block.timestamp;
+        auction_end = auction_start + _bidding_time * 1 hours;
+        STATE = auction_state.STARTED;
+        item.Brand = _brand;
+        item.Rnumber = _RNumber;
+    }
+
+    function bid() public payable an_ongoing_auction returns (bool) {
+        require(bids[msg.sender] + msg.value > highest_bid, "Error: make a higher bid");
+        highest_bidder = msg.sender;
+        highest_bid = msg.value;
+        emit BidEvent(highest_bidder, highest_bid);
+        return true;
+    }
+
+    function cancel_auction() only_owner an_ongoing_auction returns (bool) {
+        STATE = auction_state.CANCELLED;
+        CanceledEvent("Auction Cancelled", now);
+        return true;
+    }
+
+    function withdraw() public returns (bool) {
+        require(now > auction_end, "ERROR: Auction still open");
+        require(msg.sender != highest_bidder);
+        uint amount = bids[msg.sender];
+        bids[msg.sender] = 0;
+        msg.sender.transfer(amount);
+        WithdrawalEvent(msg.sender, amount);
+        return true;
+    }
+
+    // reason for not implementing the payback function which can be run and the users are paid back
+    // this can result into a lot of errors. if send and transfer fails for a particular reason, this
+    // would block this function forever, as it could get stuck ever time the function is called.
+    // a malicious user that bid only 1 wei from a contract with a non-payable fallback can block
+    // the refund process by making the transfer method fail (and therefore throw) forever, meaning
+    // that the loop will never get executed completely.
+
+}
